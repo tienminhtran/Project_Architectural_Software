9720eb2dbc6ea295d3e4c144f1b966951ab716ea
tienminhtran <tientot36@gmail.com>
2025-01-05 15:08:12 +0700

    creat-project

---
+++ Project_Architectural_Software_Group_9/src/main/java/vn/edu/iuh/fit/project_architectural_software_group_9/ProjectArchitecturalSoftwareGroup9Application.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.project_architectural_software_group_9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectArchitecturalSoftwareGroup9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectArchitecturalSoftwareGroup9Application.class, args);
+	}
+
+}
---
+++ Project_Architectural_Software_Group_9/src/test/java/vn/edu/iuh/fit/project_architectural_software_group_9/ProjectArchitecturalSoftwareGroup9ApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.project_architectural_software_group_9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectArchitecturalSoftwareGroup9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
