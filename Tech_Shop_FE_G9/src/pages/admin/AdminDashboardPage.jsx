a6326a77d9e37fdffb8a792d9ed1ec6a0d467522
TienSinh03 <zatos1232@gmail.com>
2025-03-04 22:35:59 +0700

    init project tech_shop fe

---
+++ Tech_Shop_FE_G9/src/pages/admin/AdminDashboardPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AdminDashboardPage = () => {
+  return (
+    <div>
+      <h1>Admin Dashboard Page</h1>
+    </div>
+  );
+}
+
+export default AdminDashboardPage;
