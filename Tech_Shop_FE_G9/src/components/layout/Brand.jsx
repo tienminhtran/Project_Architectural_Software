e24d2e634ef01a2271fc7319220d788aafe64eb2
tienminhtran <tientot36@gmail.com>
2025-04-09 14:52:41 +0700

    FE / user / brand render

---
+++ Tech_Shop_FE_G9/src/components/layout/Brand.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { FaTags } from "react-icons/fa";
+import "../../assets/css/Brand.css";
+import { Link } from "react-router-dom";
+import useBrandData from "../../hooks/useBrandData";
+
+const Brand = () => {
+  const { brandAll } = useBrandData();
+
+  // Dữ liệu brand lấy từ API
+  const brands = brandAll?.data?.response || [];
+
+  return (
+    <div className="brand-section">
+      <div>
+        <h2><FaTags className="brand-icon" /> Brands</h2>
+        <h1>Browse by Brands</h1>
+      </div>
+      <div className="brand-sliders">
+        <Swiper spaceBetween={20} slidesPerView={6}>
+          {brands.map((brand, index) => (
+            <SwiperSlide key={index}>
+              <div className="brand-card">
+                <img src={brand.brandImg} alt={brand.name} />
+                <p>{brand.name}</p>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
+  );
+};
+
+export default Brand;
