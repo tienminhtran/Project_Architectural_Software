c5296f6eed7b14667c624e4a91a63bae011142db
tienminhtran <tientot36@gmail.com>
2025-04-17 13:02:02 +0700

    FE giao dien bai viet

--- Tech_Shop_FE_G9/src/components/layout/HomeProductDetail.jsx
+++ Tech_Shop_FE_G9/src/components/layout/HomeProductDetail.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 import HeaderUserBasic  from './HeaderUserBasic';
 import ProductDetail from './ProductDetail';
 import ProductsYouViewed from './ProductsYouViewed';
-
+import ProductUserPhone from './ProductUserPhone';
+import ProductInformation from './ProductInformation';
+import Footer from './Footer';
 const HomeProductDetail = () => {
   return (
     <div>
         <HeaderUserBasic />
         <ProductDetail />
+        Sản phẩm tương tự
+        <ProductUserPhone />
+        <ProductInformation />
         <ProductsYouViewed />
+        <Footer />
         {/* <div className="container-fluid py-3">
             <div className="row">
             <div className="col-12 text-center">
