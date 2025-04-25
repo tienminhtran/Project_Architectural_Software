5ce0341b203859c021f709c31bd0fdb523a2323d
TienSinh03 <zatos1232@gmail.com>
2025-04-25 17:19:35 +0700

    update / connet BE / FE

--- Tech_Shop_FE_G9/src/components/layout/HeaderUserBasic.jsx
+++ Tech_Shop_FE_G9/src/components/layout/HeaderUserBasic.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState, useRef, useMemo } from "react";
 import {
   FaBars,
   FaChevronDown,
@@ -10,10 +10,30 @@ import "../../../src/assets/css/HeaderUserBasic.css";
 import AnnouncementBar from "./AnnouncementBar.jsx";
 import useUser from "../../hooks/useUser.js";
 import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { getAccessToken, removeAccessToken } from "../../services/authService.js";
+import { logout } from "../../store/slices/AuthSlice.js";
+import { confirmAlert } from 'react-confirm-alert';
+import { toast } from "react-toastify";
+import useCart from "../../hooks/useCart.js";
 
 const HeaderUserBasic = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
   const { userInfor } = useUser(0, 1);
+    const user = useMemo(() => {
+      return userInfor|| null;
+    }, [userInfor]);
+
+    const {carts} = useCart();
+    const cartItems = useMemo(() => {
+      if(!carts) return [];
+      return carts.response;
+    }, [carts]);
+
+    const token = getAccessToken();
+    
 
   const userMenuRef = useRef();
   const categoryMenuRef = useRef();
@@ -43,6 +63,32 @@ const HeaderUserBasic = () => {
     document.addEventListener("mousedown", handleClickOutside);
     return () => document.removeEventListener("mousedown", handleClickOutside);
   }, []);
+
+  const handleLogout = () => {
+    confirmAlert({
+      message: 'Bạn có chắc chắn muốn đăng xuất không?',
+      buttons: [
+        {
+          label: 'Tiếp tục',
+          onClick: () => {
+            dispatch(logout());
+            removeAccessToken();
+            localStorage.removeItem("lastDashboard");
+            navigate("/");
+            window.location.reload();
+            toast.success("Đăng xuất thành công", {
+              position: 'top-right',
+              autoClose: 2000,              
+            })
+          }
+        }, 
+        {
+          label: 'Hủy bỏ',
+          onClick: () => {}
+        }
+      ]
+    })
+  }
 
   return (
     <div className="header-user">
@@ -144,7 +190,7 @@ const HeaderUserBasic = () => {
             <div className="header-user-basic__menu-icons">
               <div className="header-user-basic__icon-item">
                 <FaShoppingCart style={{ color: "#838383" }} onClick={()=>navigate('/cart')}/>
-                <span className="header-user-basic__icon-badge-cart">2</span>
+                <span className="header-user-basic__icon-badge-cart">{cartItems.length}</span>
               </div>
               <div className="header-user-basic__icon-item">
                 <FaHeart style={{ color: "#838383" }} onClick={()=>navigate('/favorite-products')}/>
@@ -157,22 +203,38 @@ const HeaderUserBasic = () => {
               >
                 <FaUser />
                 <span className="header-user-basic__username">
-                  {userInfor?.firstname || "User"}
+                  {user?.firstname || "User"}
                 </span>
                 {showMenu && (
-                  <div className="header-user-basic__dropdown">
-                    <div
-                      className="header-user-basic__dropdown-item"
-                      onClick={() => navigate("/login")}
-                    >
-                      Login
-                    </div>
-                    <div
-                      className="header-user-basic__dropdown-item"
-                      onClick={() => navigate("/")}
-                    >
-                      Logout
-                    </div>
+                  <div className="header-user__dropdown">
+                    {!token ? (
+
+                      <div className="p-3" >
+                        <p className="">Xin chào, vui lòng đăng nhập</p>
+                        <button className="btn btn-primary rounder-3"  onClick={() => navigate('/login')}> 
+                          Login
+                        </button>
+                        <div className="d-flex justify-content-center align-items-center mt-2 gap-2" style={{ width: '100%' }}>
+                          <span className="text-muted"style={{fontSize: '12px'}}>No account yet?</span>
+                          <button className="btn btn-danger w-50" onClick={() => navigate('/register')}>Register</button>
+                        </div>
+                      </div>
+
+                    ) : (
+                      <div>
+                        <div className="header-user__dropdown-item" onClick={() => navigate('/my-account')} >My Account</div>
+                        
+                        {userInfor.role.code === "ADMIN" && (
+                            <div className="header-user__dropdown-item" onClick={() => navigate('/admin/dashboard')} >Admin</div>
+                        )}
+
+                        {(userInfor.role.code === "ADMIN" || userInfor.role.code === "MANAGER") && (
+                          <div className="header-user__dropdown-item" onClick={() => navigate('/manager/dashboard')} >Manager</div>
+                        )}
+
+                        <div className="header-user__dropdown-item" onClick={handleLogout} >Logout</div>
+                      </div>
+                    )}
                   </div>
                 )}
               </div>
