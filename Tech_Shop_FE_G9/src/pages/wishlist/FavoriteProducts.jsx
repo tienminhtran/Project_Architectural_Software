6c72bab36ff60365e6a490d437ef2e567126fe2a
Duong-GB <duongnguyenqn1323@gmail.com>
2025-05-19 15:23:06 +0700

    [update] fix add product to wishlit and cart in pageCategory

--- Tech_Shop_FE_G9/src/pages/wishlist/FavoriteProducts.jsx
+++ Tech_Shop_FE_G9/src/pages/wishlist/FavoriteProducts.jsx
@@ -61,6 +61,7 @@ export default function FavoriteProducts() {
     }
   };
 
+  // Hàm xử lý xóa sản phẩm khỏi danh sách yêu thích
   const handleRemoveFavorite = (id) => {
     confirmAlert({
       title: "Xác nhận xóa",
